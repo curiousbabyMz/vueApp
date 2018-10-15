c25368dd14cded5918690a9efb6e9e9ee2144351
Mz <curiousbabyMz@gmail.com>
2018-10-16 01:10:51 +0800

    home's edit

---
+++ src/components/film.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="film">
+        <h3 class="film_type">
+            <span>{{type}}</span>
+            <router-link :to='{path:"/classify/"+url}'><span class="more"><em>更多</em></span></router-link>
+        </h3>
+        <div class="film_list" :ref="el" :data-request='url'>
+            <ul class="clearfix">
+                <router-link tag='li' v-for="(v) in array" :key="v.id" :to='{path:"/film-detail/"+v.id}'>
+                <div class="film_list_img"></div>
+                <div class="film__list__detail">
+                        <h4 class="film__list__title">{{v.title}}</h4>
+                        <p class="film__list__rank">评分：{{v.rating.average}}</p>
+                        <p class="film__list__rank">
+                            <span :class="{rankColor:v.rating.average>((i-0.5)*2)}" class="iconfont icon-rank" v-for="i in 5" :key="i"></span>
+                        </p>
+                    </div>
+                </router-link>
+            </ul>
+        </div>
+    </div>
+</template>
